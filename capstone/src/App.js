09ea908f934dfd7b807ebeda25b302b5acdbba7a
Abhi-2306 <abhijtih.pandiri@gmail.com>
2024-01-25 22:05:43 +0530

    metatags

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header.js';
+import Footer from './Footer.js';
 function App() {
   return (
     <div className="App">
-      Home Page
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ capstone/src/Footer.js
@@ -0,0 +1,35 @@
+
+function Footer(){
+    return(
+        <footer>
+            <img src="" alt="" />
+            <div class="navlinks">
+                <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="Menu">Menu</a></li>
+                <li><a href="Reservations">Reservations</a></li>
+                <li><a href="Order Online">Order Online</a></li>
+                <li><a href="Login">Login</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="/"> Address</a></li>
+                    <li><a href="/">Phone Number</a></li>
+                    <li><a href="/">Email</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="/">Twitter</a></li>
+                    <li><a href="/">Instagram</a></li>
+                    <li><a href="/">Facebook</a></li>
+                </ul>
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
